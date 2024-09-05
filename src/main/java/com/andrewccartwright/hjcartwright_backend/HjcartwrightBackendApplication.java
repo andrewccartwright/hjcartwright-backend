47108c6e71534be4d3348029ca2ae62d37dcf74c
drewcartwright-sunnystreet <drew.cartwright@sunnystreet.com>
2024-09-05 10:14:18 -0600

    create backend

---
+++ src/main/java/com/andrewccartwright/hjcartwright_backend/HjcartwrightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.andrewccartwright.hjcartwright_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjcartwrightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjcartwrightBackendApplication.class, args);
+	}
+
+}
