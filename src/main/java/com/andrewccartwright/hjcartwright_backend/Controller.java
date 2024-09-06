b4a34ab1167208ac6f610e13447312b95ca5b6a9
drewcartwright-sunnystreet <drew.cartwright@sunnystreet.com>
2024-09-05 20:21:48 -0600

    remove cross origin check

--- src/main/java/com/andrewccartwright/hjcartwright_backend/Controller.java
+++ src/main/java/com/andrewccartwright/hjcartwright_backend/Controller.java
@@ -1,6 +1,5 @@
 package com.andrewccartwright.hjcartwright_backend;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     
     @GetMapping("/api_key")
-    @CrossOrigin(origins = "hjcartwright")
     public String getApiKey() {
         return System.getenv("INSTAGRAM_API_KEY");
     }
